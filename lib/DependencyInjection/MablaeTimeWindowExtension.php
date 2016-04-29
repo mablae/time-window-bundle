9fe4ccc7beff39724f255ed8e325b7da6bd374f4
Malte Blättermann <mbl@svb.de>
2016-04-29 14:30:27 +0200

    Fiix PSR-4 autoloading

--- lib/DependencyInjection/MablaeTimeWindowExtension.php
+++ lib/DependencyInjection/MablaeTimeWindowExtension.php
@@ -26,7 +26,7 @@ class MablaeTimeWindowExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
 
         $this->loadTimeWindows($container, $config);
